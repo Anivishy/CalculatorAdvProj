d13c1101ca49fa8fa1b7ef370f6d961ee3336d1c
Anivishy <ani.vishy@outlook.com>
2023-10-03 13:08:41 -0700

    Setup quit loop

--- Calculator.java
+++ Calculator.java
@@ -15,59 +15,70 @@ public class Calculator {
         Scanner scanner=new Scanner(System.in);
         Engine engine=new Engine();
         Formatter formatter=new Formatter();
-
-        System.out.println("Enter an expression to compute or type 'q' to quit: ");
-        String expression=scanner.nextLine();
-
         String ans = "";
+
+        System.out.println("Enter an expression to compute or type '#' to quit: ");
+        String expression=scanner.nextLine();
 
         //remove all whitespace form input string
         expression.replace("\\s", "");
-
-        String currentExpression=expression;
-        Translator translator = new Translator(expression);
-
-        steps.add(expression);
-
-        while (!currentExpression.matches("-?\\d+(\\.\\d+)?")) {
-
-            ArrayList<String> components = translator.parse(currentExpression);
-            System.out.println(components.toString());
-            engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
-            double result=engine.compute();
-            currentExpression=currentExpression.replace(components.get(3), Double.toString(result));
-            steps.add(currentExpression);
-
+        for (int i = 1; i < expression.length();  i ++){
+            if (expression.charAt(i) == '(' || expression.charAt(i) == ')'){
+                if (expression.charAt(i - 1) != '+'){
+                    break;
+                }
+            }
         }
-        
-        ans = steps.get(steps.size() - 1);
-        formatter.formatAndDisplay(steps);
+        while (true) {
+            if (expression == "#"){
+                break;
+            }
 
-        // Testing out Translator PEMDAS functions here
+            else{
+                String currentExpression=expression;
+                Translator translator = new Translator(expression);
 
-        /** 
-        ArrayList<Object> checkPar = translator.checkParenthesis(expression);
-        System.out.println("Check Parenthesis: " + checkPar.toString() + "\n");
+                steps.add(expression);
 
-        ArrayList<String> checkExp = translator.checkExponents(expression);
-        System.out.println("Check Exponents: " + checkExp.toString() + "\n");
-        
-        ArrayList<String> checkMult = translator.checkMultiplication(expression);
-        System.out.println("Check Multiplication: " + checkMult.toString() + "\n");
+                while (!currentExpression.matches("-?\\d+(\\.\\d+)?")) {
 
-        ArrayList<String> checkDiv = translator.checkDivision(expression);
-        System.out.println("Check Division: " + checkDiv.toString() + "\n");
+                    ArrayList<String> components = translator.parse(currentExpression);
+                    System.out.println(components.toString());
+                    engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
+                    double result=engine.compute();
+                    currentExpression=currentExpression.replace(components.get(3), Double.toString(result));
+                    steps.add(currentExpression);
 
-        ArrayList<String> checkAdd = translator.checkAddition(expression);
-        System.out.println("Check Addition: " + checkAdd.toString() + "\n");
+                }
 
-        ArrayList<String> checkSub = translator.checkSubtraction(expression);
-        System.out.println("Check Subtraction: " + checkSub.toString() + "\n");
-        **/
+                ans = steps.get(steps.size() - 1);
+                formatter.formatAndDisplay(steps);
+
+                // Testing out Translator PEMDAS functions here
+
+                /** 
+                ArrayList<Object> checkPar = translator.checkParenthesis(expression);
+                System.out.println("Check Parenthesis: " + checkPar.toString() + "\n");
+
+                ArrayList<String> checkExp = translator.checkExponents(expression);
+                System.out.println("Check Exponents: " + checkExp.toString() + "\n");
+                
+                ArrayList<String> checkMult = translator.checkMultiplication(expression);
+                System.out.println("Check Multiplication: " + checkMult.toString() + "\n");
+
+                ArrayList<String> checkDiv = translator.checkDivision(expression);
+                System.out.println("Check Division: " + checkDiv.toString() + "\n");
+
+                ArrayList<String> checkAdd = translator.checkAddition(expression);
+                System.out.println("Check Addition: " + checkAdd.toString() + "\n");
+
+                ArrayList<String> checkSub = translator.checkSubtraction(expression);
+                System.out.println("Check Subtraction: " + checkSub.toString() + "\n");
+                **/
+                
+            }
+        }
        
-    
-
-
     }
     
     
