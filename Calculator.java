2901e05871b9a60111e36b36c6daddc8815fa61a
Anivishy <ani.vishy@outlook.com>
2023-10-10 13:00:08 -0700

    Bugfixed implied multiplication

--- Calculator.java
+++ Calculator.java
@@ -20,6 +20,7 @@ public class Calculator {
         
 
         Boolean continue_loop = true;
+        Boolean modified = false;
 
         //remove all whitespace form input string
         
@@ -30,19 +31,22 @@ public class Calculator {
             expression.replace("\\s", "");
             for (int i = 1; i < expression.length();  i ++){
                 if (expression.charAt(i) == '('){
-                    System.out.println("______________________________");
                     String prev_char = expression.charAt(i - 1) + "";
                     if (!(prev_char.equals("+")) || !(prev_char.equals("-")) || !(prev_char.equals("*")) || !(prev_char.equals("/")) || !(prev_char.equals("^"))){
+                        updatedString = "";
                         updatedString += expression.substring(0, i);
                         updatedString += "*";
                         updatedString += expression.substring(i);
                         System.out.println(updatedString);
+                        modified = true;
                     }
                 }
 
-                else{
+                else if (!modified){
                     updatedString = expression;
                 }
+
+                
             }
 
             //System.out.print(expression);
@@ -64,6 +68,7 @@ public class Calculator {
                     engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
                     double result=engine.compute();
                     currentExpression=currentExpression.replace(components.get(3), Double.toString(result));
+                    System.out.println(currentExpression);
                     steps.add(currentExpression);
 
                 }
