12bf3fe948ecdf2efd62f7d45f944a6c946bc137
Anivishy <ani.vishy@outlook.com>
2023-10-19 08:39:18 -0700

    Completed + debugged log calculation function

--- Calculator.java
+++ Calculator.java
@@ -12,8 +12,8 @@ public class Calculator {
 
         ArrayList<String> steps = new ArrayList<String>();
 
+        Translator translator = new Translator(expression);
         Engine engine=new Engine();
-
         ExtraFunctions extra = new ExtraFunctions();
    
         //used for handling implied multiplication
@@ -27,41 +27,36 @@ public class Calculator {
         updatedString = "";
         //Remove Spacing
         expression = expression.replace("\\s", "");
+        System.out.println("Intial expression:" + expression);
 
         //Remove special chars
         expression = extra.replacePi(expression);
         expression = extra.replaceE(expression);
 
+        //simplify logs, ln sqrt, etc.
+        //System.out.println(expression);
+        expression = extra.simplifyLog(expression);
+        System.out.println(expression);
+
         //implied multiplication
         expression = extra.impliedMulti(expression);
 
         //negative 
-        
-        for (int i = 1; i < expression.length(); i++){
-            if (expression.charAt(i) == '-' && Character.isDigit(expression.charAt(i - 1))){
-                updatedString = "";
-                updatedString += expression.substring(0, i);
-                updatedString += "+";
-                updatedString += expression.substring(i);
-                expression = updatedString;
+        expression = extra.updateNegatives(expression);
 
-            }
-        }
 
         String currentExpression=expression;
-        Translator translator = new Translator(expression);
 
         steps.add(expression);
 
         while (!currentExpression.matches("-?\\d+(\\.\\d+)?")) {
-
+            //expression = extra.updateNegatives(expression);
             ArrayList<String> components = translator.parse(currentExpression);
             //System.out.println(components.toString());
             engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
             double result=engine.compute();
             currentExpression=currentExpression.replace(components.get(3), String.format("%.2f", result));
             steps.add(currentExpression);
-
         }
 
         return steps;
@@ -69,100 +64,103 @@ public class Calculator {
         
     }
 
-    public static void main(String[] args) {
-        ArrayList<String> steps = new ArrayList<String>();
-
-        Scanner scanner=new Scanner(System.in);
-        Engine engine=new Engine();
-        Formatter formatter=new Formatter();
-        String ans = "";
-        //used for handling implied multiplication
-        String updatedString = "";
-        
-
-        Boolean continue_loop = true;
-        Boolean modified = false;
-
-        //remove all whitespace form input string
-
-        
-        
-        while (continue_loop) {            
-            System.out.println("Enter an expression to compute or type '#' to quit: ");
-            String expression=scanner.nextLine();
-            modified = false;
-            updatedString = "";
-
-            // while(expression.contains("(")){
-            //     String checkString = expression.substring(expression.indexOf("("), expression.indexOf(")"));
-            //     if (checkString.contains("+") || checkString.contains("*") || checkString.contains("/") || checkString.contains("^")){
-            //         ;
-            //     }
-                
-            // }
-
-            expression = expression.replace("\\s", "");
-            for (int i = 1; i < expression.length();  i ++){
-                if (expression.charAt(i) == '('){
-                    String prev_char = expression.charAt(i - 1) + "";
-                    if (!(prev_char.equals("+")) && !(prev_char.equals("-")) && !(prev_char.equals("*")) && !(prev_char.equals("/")) && !(prev_char.equals("^"))){
-                        updatedString = "";
-                        updatedString += expression.substring(0, i);
-                        updatedString += "*";
-                        updatedString += expression.substring(i);
-                        expression = updatedString;
-                        modified = true;
-                    }
-                }
-
-                else if (!modified){
-                    //updatedString = expression;
-                    expression = expression;
-                }                
-            }
-
-            for (int i = 1; i < expression.length(); i++){
-                if (expression.charAt(i) == '-' && Character.isDigit(expression.charAt(i - 1))){
-                    updatedString = "";
-                    updatedString += expression.substring(0, i);
-                    updatedString += "+";
-                    updatedString += expression.substring(i);
-                    expression = updatedString;
-
-                }
-            }
-
-            //System.out.print(expression);
-
-            if (expression.equals("#")){
-                continue_loop = false;
-            }
-
-            else{
-                String currentExpression=expression;
-                Translator translator = new Translator(expression);
-
-                steps.add(expression);
-
-                while (!currentExpression.matches("-?\\d+(\\.\\d+)?")) {
-
-
-                    ArrayList<String> components = translator.parse(currentExpression);
-                    //System.out.println(components.toString());
-                    engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
-                    double result=engine.compute();
-                    currentExpression=currentExpression.replace(components.get(3), String.format("%.2f", result));
-                    steps.add(currentExpression);
-
-                }
-
-                ans = steps.get(steps.size() - 1);
-                formatter.formatAndDisplay(steps);
-                steps.clear();
-            }
-        }
-       
-    }
-    
-    
 }
+
+    //********************OLD CONSOLE IMPLEMENTATION*************************
+//     public static void main(String[] args) {
+//         ArrayList<String> steps = new ArrayList<String>();
+
+//         Scanner scanner=new Scanner(System.in);
+//         Engine engine=new Engine();
+//         Formatter formatter=new Formatter();
+//         String ans = "";
+//         //used for handling implied multiplication
+//         String updatedString = "";
+        
+
+//         Boolean continue_loop = true;
+//         Boolean modified = false;
+
+//         //remove all whitespace form input string
+
+        
+        
+//         while (continue_loop) {            
+//             System.out.println("Enter an expression to compute or type '#' to quit: ");
+//             String expression=scanner.nextLine();
+//             modified = false;
+//             updatedString = "";
+
+//             // while(expression.contains("(")){
+//             //     String checkString = expression.substring(expression.indexOf("("), expression.indexOf(")"));
+//             //     if (checkString.contains("+") || checkString.contains("*") || checkString.contains("/") || checkString.contains("^")){
+//             //         ;
+//             //     }
+                
+//             // }
+
+//             expression = expression.replace("\\s", "");
+//             for (int i = 1; i < expression.length();  i ++){
+//                 if (expression.charAt(i) == '('){
+//                     String prev_char = expression.charAt(i - 1) + "";
+//                     if (!(prev_char.equals("+")) && !(prev_char.equals("-")) && !(prev_char.equals("*")) && !(prev_char.equals("/")) && !(prev_char.equals("^"))){
+//                         updatedString = "";
+//                         updatedString += expression.substring(0, i);
+//                         updatedString += "*";
+//                         updatedString += expression.substring(i);
+//                         expression = updatedString;
+//                         modified = true;
+//                     }
+//                 }
+
+//                 else if (!modified){
+//                     //updatedString = expression;
+//                     expression = expression;
+//                 }                
+//             }
+
+//             for (int i = 1; i < expression.length(); i++){
+//                 if (expression.charAt(i) == '-' && Character.isDigit(expression.charAt(i - 1))){
+//                     updatedString = "";
+//                     updatedString += expression.substring(0, i);
+//                     updatedString += "+";
+//                     updatedString += expression.substring(i);
+//                     expression = updatedString;
+
+//                 }
+//             }
+
+//             //System.out.print(expression);
+
+//             if (expression.equals("#")){
+//                 continue_loop = false;
+//             }
+
+//             else{
+//                 String currentExpression=expression;
+//                 Translator translator = new Translator(expression);
+
+//                 steps.add(expression);
+
+//                 while (!currentExpression.matches("-?\\d+(\\.\\d+)?")) {
+
+
+//                     ArrayList<String> components = translator.parse(currentExpression);
+//                     //System.out.println(components.toString());
+//                     engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
+//                     double result=engine.compute();
+//                     currentExpression=currentExpression.replace(components.get(3), String.format("%.2f", result));
+//                     steps.add(currentExpression);
+
+//                 }
+
+//                 ans = steps.get(steps.size() - 1);
+//                 formatter.formatAndDisplay(steps);
+//                 steps.clear();
+//             }
+//         }
+       
+//     }
+    
+    
+// }
--- ExtraFunctions.java
+++ ExtraFunctions.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ExtraFunctions {
+
+public class ExtraFunctions {    
+
 
     //Updating universal constants
 
@@ -24,7 +26,7 @@ public class ExtraFunctions {
         for (int i = 1; i < expression.length();  i ++){
             if (expression.charAt(i) == '('){
                 String prev_char = expression.charAt(i - 1) + "";
-                if (!(prev_char.equals("+")) && !(prev_char.equals("-")) && !(prev_char.equals("*")) && !(prev_char.equals("/")) && !(prev_char.equals("^"))){
+                if (!(prev_char.equals("+")) && !(prev_char.equals("-")) && !(prev_char.equals("*")) && !(prev_char.equals("/")) && !(prev_char.equals("^")) && !(prev_char.equals("("))){
                     updatedString = "";
                     updatedString += expression.substring(0, i);
                     updatedString += "*";
@@ -36,7 +38,93 @@ public class ExtraFunctions {
         return expression;   
     }
 
+    public String updateNegatives(String expression){
+        String updatedString = "";
+        for (int i = 1; i < expression.length(); i++){
+            if (expression.charAt(i) == '-' && Character.isDigit(expression.charAt(i - 1))){
+                updatedString = "";
+                updatedString += expression.substring(0, i);
+                updatedString += "+";
+                updatedString += expression.substring(i);
+                expression = updatedString;
 
+            }
+        }
+        return expression;
+    }
+
+    //log, ln, and square root
+    public String simplifyLog(String expression){
+        while (expression.contains("log")){
+            String tempExpression = expression.substring(expression.indexOf("l"), expression.lastIndexOf(")") + 1);
+            System.out.print(tempExpression);
+            Integer finalIndex = 0;
+            Integer paraCount = 0;
+                Boolean paraDetect = false;
+            for (int i = 0; i < tempExpression.length(); i++){
+                if (tempExpression.charAt(i) == '('){
+                    paraCount += 1;
+                    paraDetect = true;
+                }
+                System.out.println(paraCount);
+                if (tempExpression.charAt(i) == ')'){
+                    System.out.println("hi");
+                    paraCount -= 1;
+                }
+                if (paraDetect){
+                    if (paraCount == 0){
+                        finalIndex = i;
+                    }
+                }
+                System.out.println("final index:" + finalIndex);
+            }
+            
+            String curLog = expression.substring(expression.indexOf("l"), finalIndex + 1);
+            String innerExpression = curLog.substring(curLog.indexOf("(") + 1, curLog.lastIndexOf(")"));
+            String finalResult = "";
+            ArrayList <String> result = Calculator.compute(innerExpression);
+            finalResult = Double.toString(Math.log10(Double.parseDouble(result.get(result.size() - 1))));
+            expression = expression.replace(curLog, finalResult);
+        }
+        
+        return expression;
+    }
+
+    public String simplifyLn(String expression){
+        while (expression.contains("log")){
+            String curLog = expression.substring(expression.indexOf("l"), expression.indexOf(")") + 1);
+            String innerExpression = curLog.substring(curLog.indexOf("("), curLog.indexOf(")") + 1);
+            String finalResult = "";
+            ArrayList <String> result = Calculator.compute(innerExpression);
+            finalResult = Double.toString(Math.log(Double.parseDouble(result.get(result.size() - 1))));
+            expression = expression.replace(curLog, finalResult);
+        }
+        
+        return expression;
+    }
+
+    public String simplifySqrt(String expression){
+        Translator translator = new Translator(expression);
+        Engine engine=new Engine();
+        while (expression.contains("sqrt")){
+            String curLog = expression.substring(expression.indexOf("l"), expression.indexOf(")") + 1);
+            String innerExpression = curLog.substring(curLog.indexOf("("), curLog.indexOf(")") + 1);
+            String finalResult = "";
+            while (!innerExpression.matches("-?\\d+(\\.\\d+)?")) {
+                //expression = extra.updateNegatives(expression);
+                ArrayList<String> components = translator.parse(innerExpression);
+                //System.out.println(components.toString());
+                engine.setExpression(components.get(0), Double.parseDouble(components.get(1)), Double.parseDouble(components.get(2)));
+                double result=engine.compute();
+                innerExpression=innerExpression.replace(components.get(3), String.format("%.2f", result));
+                //steps.add(innerExpression);
+            }
+            finalResult = Double.toString(Math.log(Double.parseDouble(innerExpression)));
+            expression = expression.replace(curLog, finalResult);
+        }
+        
+        return expression;
+    }
 
 }
 
